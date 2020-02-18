6d71713ee70f87ef1fcbdb6f0f42a5183dc16a5c
林添耀 <lintianyao@outlook.com>
2020-02-18 21:54:22 +0800

    init

---
+++ client-main/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "github.com/go-telegram-bot-api/telegram-bot-api"
+    "log"
+    "net/http"
+    "os"
+)
+
+func main() {
+    botToken := os.Getenv("BOT_TOKEN")
+    botHost := os.Getenv("BOT_HOST")
+    botPort := os.Getenv("BOT_PORT")
+
+    bot, err := tgbotapi.NewBotAPI(botToken)
+    if err != nil {
+        log.Fatal(err)
+    }
+    bot.Debug = true
+
+    _, err = bot.SetWebhook(tgbotapi.NewWebhook(botHost + ":" + botPort + "/" + bot.Token))
+    if err != nil {
+        log.Fatal(err)
+    }
+    info, err := bot.GetWebhookInfo()
+    if err != nil {
+        log.Fatal(err)
+    }
+    if info.LastErrorDate != 0 {
+        log.Printf("Telegram回调错误: %s", info.LastErrorMessage)
+    }
+    updates := bot.ListenForWebhook("/" + bot.Token)
+    go http.ListenAndServe("0.0.0.0:"+botPort, nil)
+
+    for update := range updates {
+        log.Printf("%+v\n", update)
+    }
+}
