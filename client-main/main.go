61b7486db348ec04edecd07a46e16689a192373b
林添耀 <lintianyao@outlook.com>
2020-02-19 11:04:23 +0800

    取消webhook

--- client-main/main.go
+++ client-main/main.go
@@ -3,43 +3,27 @@ package main
 import (
     "github.com/go-telegram-bot-api/telegram-bot-api"
     "log"
-    "net/http"
     "os"
 )
 
 func main() {
     botToken := os.Getenv("BOT_TOKEN")
-    botHost := os.Getenv("BOT_HOST")
-    botPort := os.Getenv("BOT_PORT")
 
     bot, err := tgbotapi.NewBotAPI(botToken)
     if err != nil {
         log.Fatal(err)
     }
     bot.Debug = true
+
     _, err = bot.RemoveWebhook()
-    history, err := bot.GetUpdatesChan(tgbotapi.UpdateConfig{})
-    if history != nil {
-        log.Println("清除WebHook")
-        history.Clear()
-    }
-    webHook := botHost + "/" + bot.Token
-    log.Printf("WebHook为： %s\n", webHook)
-    _, err = bot.SetWebhook(tgbotapi.NewWebhook(webHook))
-    if err != nil {
-        log.Fatal(err)
-    }
-    _, err = bot.GetWebhookInfo()
-    if err != nil {
-        log.Fatal(err)
-    }
-    //if info.LastErrorDate != 0 {
-    //    log.Printf("Telegram回调错误: %s", info.LastErrorMessage)
-    //}
-    updates := bot.ListenForWebhook("/" + bot.Token)
-    go http.ListenAndServe("0.0.0.0:"+botPort, nil)
+    config := tgbotapi.UpdateConfig{}
+    config.Timeout = 60
+    updates, err := bot.GetUpdatesChan(config)
 
     for update := range updates {
+        if update.Message == nil {
+            continue
+        }
         log.Printf("%+v\n", update)
     }
 }
